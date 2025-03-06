fe39e73b5357aa292a284cc20d025453ad1a98ef
dpencross501 <dan.pencross@nationalhighways.co.uk>
2025-03-06 10:15:02 +0000

    implement function to retrieve and display Pokémon information from PokeAPI

--- pokeapi.py
+++ pokeapi.py
@@ -1,2 +1,25 @@
 # connecting to an API using Python
 import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}pokemon/{name}"
+    response = requests.get(url)
+    print(response)
+    
+    if response.status_code == 200:
+        print("Data retrieved successfully!")
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retreive data {response}")
+    
+pokemon_name = "mew"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info['name'].capitalize()}")
+    print(f"Id: {pokemon_info['id']}")
+    print(f"Height: {pokemon_info['height']}")
+    print(f"Weight: {pokemon_info['weight']}")
